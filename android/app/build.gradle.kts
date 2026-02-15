5ec774a1a6b681e3b627ddd939d4af3e5368a9e5
cristian10gf <cristiandavidgf@gmail.com>
2026-02-14 19:39:26 -0500

    Update build.gradle.kts to upgrade build tools to version 35.0.0 and enable core library desugaring. Add dependency for desugaring JDK libs to support modern Java features.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,10 +8,11 @@ plugins {
 android {
     namespace = "com.dispoun.dispoun_app"
     compileSdk = flutter.compileSdkVersion
-    buildToolsVersion = "34.0.0"
+    buildToolsVersion = "35.0.0"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -38,6 +39,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
